a0b8a4d18e97b98f67fb75d727139c46a4e1b369
Your Name201~ <abolore200@gmail.com>
2023-06-08 21:47:17 +0100

    completed footer

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -5,3 +5,7 @@ menuBar.addEventListener('click', () => {
     menuhome.classList.toggle('show')
     body.classList.toggle('fixed')
 })
+
+const year = new Date().getFullYear()
+const span = document.querySelector('.footer-head .follow .copyright p span')
+span.innerHTML = year
