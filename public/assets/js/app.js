3db4dcbc5744425849c9f2eb7455d37da7029d56
Your Name201~ <abolore200@gmail.com>
2023-06-09 13:43:46 +0100

    completed

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -9,3 +9,13 @@ menuBar.addEventListener('click', () => {
 const year = new Date().getFullYear()
 const span = document.querySelector('.footer-head .follow .copyright p span')
 span.innerHTML = year
+
+const link = document.querySelectorAll('.whereismyloop-sub-menu ul li')
+link.forEach(onelink => {
+    onelink.addEventListener('click', () => {
+        if(menuhome.classList.contains('show') && document.body.classList.contains('fixed')){
+            menuhome.classList.remove('show')
+            document.body.classList.remove('fixed')
+        }
+    })
+})
